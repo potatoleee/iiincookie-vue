9a50824c5baf338cea84797e62d34de7c3ebd7bb
potatolee <dean19990226@gmail.com>
2023-02-23 16:01:17 +0800

    check

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,20 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
