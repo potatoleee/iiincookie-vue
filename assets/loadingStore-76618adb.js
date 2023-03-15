99a2ea73aa996ce48606d2c7125148d5fd68608b
potatolee <dean19990226@gmail.com>
2023-03-16 01:32:13 +0800

    deploy

---
+++ assets/loadingStore-76618adb.js
@@ -0,0 +1,1 @@
+import{d as o}from"./pinia-a2ec2acd.js";const e=o("loadingStore",{state:()=>({loadingItem:"",isLoading:!0})});export{e as l};
--- assets/loadingStore-cfb944cc.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./pinia-a2ec2acd.js";const t=o("loadingStore",{state:()=>({loadingItem:"",isLoading:!1})});export{t as l};
