786b596b9e4ab146b1c12c595d5de4c3e2a2981f
potatolee <dean19990226@gmail.com>
2023-03-29 16:01:53 +0800

    修改icon vite.config base

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+/* global process */
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/iiincookie-vue/",
+  base: process.env.NODE_ENV === "production" ? "/iiincookie-vue/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
